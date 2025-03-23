afa985f3581438caf9442698e522ca65707be200
Maulana Jidan Azizi <idanaf3@gmail.com>
2025-03-24 01:10:57 +0700

    First commit

---
+++ tpmodul6/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using tpmodul6;
+
+class Program {
+    static void Main() {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial by Jidan - Maulana Jidan Azizi");
+        video.PrintVideoDetails();
+
+        video.IncreasePlayCount(5000);
+        video.PrintVideoDetails();
+    }
+}
---
+++ tpmodul6/SayaTubeVideo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6
+{
+    class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title) {
+            Debug.Assert(!string.IsNullOrEmpty(title) && title.Length <= 1--, "Judul video tidak boleh null dan maksimal 100 karakter.");
+            Random r = new Random();
+            this.id = r.Next(10000, 99999);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            if (count > 10000000)
+            {
+                throw new ArgumentException("Jumlah play count maksimal adalah 10.000.000.");
+            }
+
+            try
+            {
+                checked
+                {
+                    this.playCount += count;
+                }
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Error: Play count melebihi batas.");
+            }
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine($"Video ID: {id}");
+            Console.WriteLine($"Title: {title}");
+            Console.WriteLine($"Play Count: {playCount}");
+        }
+    }
+}
